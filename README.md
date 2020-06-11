5e4f04451681f6abf4e25459e752818196073cd9
BekahT <Rebekah.treadwell@gmail.com>
2020-06-11 18:02:00 -0400

    Finish component setup

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
 This project was completed as part of the Angular: The Complete Guide course on UDemy (https://www.udemy.com/course/the-complete-guide-to-angular-2/) and includes both a recipe book and shopping list utilizing Angular 9 and Bootstrap 4.
+
+Icons made by Freepick on www.flaticon.com (https://www.flaticon.com/authors/freepik)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HeaderComponent } from './header/header.component';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule
