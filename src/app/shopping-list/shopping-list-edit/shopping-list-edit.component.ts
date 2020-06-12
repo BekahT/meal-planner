ac3516de2bebe9e72baafecfc6b56b2df84a76dc
BekahT <Rebekah.treadwell@gmail.com>
2020-06-11 22:09:49 -0400

    Initial shopping-list - finished Basics Section

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -11,5 +11,4 @@ export class ShoppingListEditComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("bread", 2),
+    new Ingredient("egg", 3),
+    new Ingredient("cinnamon", 1)
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
